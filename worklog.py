83cd3da1d6b22fca64657729fe2f06d69ffbd789
littleed <andrewdgcaldwell@gmail.com>
2017-12-10 12:22:49 +0000

    Comments and formatting

--- worklog.py
+++ worklog.py
@@ -14,26 +14,30 @@ def callForJSON(url, field):
 		response.raise_for_status()
 
 ##Initial values
+site='https://littleed.atlassian.net'
 headers = {'Content-type': 'application/json'}
 
 ##Get credentials
 username=input("Username:")
 password=getpass.getpass()
 
-##Get Project and Sprint
+##Get Project
 project=input("Project Key:") or "TES"
-url='https://littleed.atlassian.net/rest/agile/1.0/board?type=scrum&projectKeyOrId='+project
+##Get Board
+url=site + '/rest/agile/1.0/board?type=scrum&projectKeyOrId='+project
 print("ID\tBoard Name")
 for board in callForJSON(url, "values"):
 	print(str(board["id"]) + "\t" + board["name"])
-	
 board=input("BoardID:")
-url='https://littleed.atlassian.net/rest/agile/1.0/board/' + board + '/sprint'
+
+##Get Sprint
+url=site + '/rest/agile/1.0/board/' + board + '/sprint'
 print("ID\tSprint Name")
 for sprint in callForJSON(url, "values"):
 	print(str(sprint["id"]) + "\t" + sprint["name"])
 sprint=input("SprintID:")
 
+##Prep Spreadsheet
 book = xlwt.Workbook(encoding="utf-8")
 sheet1 = book.add_sheet("Worklog")
 print("KEY\tUSER\tTIME_SPENT(seconds)")
@@ -42,11 +46,12 @@ sheet1.write(i,0, "Issue")
 sheet1.write(i,1, "Developer")
 sheet1.write(i,2, "Time Spent")
 
-url = 'https://littleed.atlassian.net/rest/agile/1.0/sprint/' + sprint + '/issue'
+##Get Issues in sprint
+url = site + '/rest/agile/1.0/sprint/' + sprint + '/issue'
 for issue in callForJSON(url, "issues"):
 	key = issue["key"]
-	##Get Worklog
-	url = 'https://littleed.atlassian.net/rest/api/2/issue/' + key + '/worklog'
+	##Get Worklogs for issue
+	url = site + '/rest/api/2/issue/' + key + '/worklog'
 	for log in callForJSON(url, "worklogs"):
 		i=i+1
 		sheet1.write(i,0, key)
@@ -54,8 +59,9 @@ for issue in callForJSON(url, "issues"):
 		sheet1.write(i,2, log["timeSpentSeconds"])
 		print(key + "\t" + log["author"]["key"] + "\t" + str(log["timeSpentSeconds"]))
 
+##Save Spreadsheet
 book.save("worklog.xls")
-print("Written to " + worklog.xls)
+print("Written to worklog.xls")
 
 	
 	
